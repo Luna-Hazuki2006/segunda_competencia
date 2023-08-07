7ed2aa9dac0624a16040f0e70429881c5c25cc22
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-08-07 09:14:11 -0400

    todo es inutil

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-const mongo = require('mongodb').MongoClient
-const url = 'mongodb+srv://lunahazuki2006:cXU0lYhSncWZ12FM@cluster0.owjghpf.mongodb.net/'
+const { MongoClient } = require("mongodb")
 
+const url = 'mongodb+srv://lunahazuki2006:cXU0lYhSncWZ12FM@cluster0.owjghpf.mongodb.net/'
+const tabla = document.getElementById('tabla')
+function mostrar() {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err
+        let transformacion = db.db('estudiantes')
+        transformacion.collection('estudiantes').find(function(err, result) {
+            if (err) throw err
+            console.log(result);
+            llenar(result)
+            db.close()
+        })
+    })
+}
+function llenar(objeto) {
+    let texto = ''
+    for (const cosa in objeto) {
+        texto += '<tr>'
+        texto += '<td>' + cosa['nombre'] + '</td>'
+        texto += '<td>' + cosa['edad'] + '</td>'
+        texto += '<td>' + cosa['frase'] + '</td>'
+        texto += '</tr>'
+    }
+    tabla.innerText += texto
+}
+
+mostrar()
