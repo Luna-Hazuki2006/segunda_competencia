077dbbeffc6e38a2306c92b940a15539c6159b47
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-08-07 10:21:08 -0400

    funcionoooooooooooooooooooooooo

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-console.log('hola mundo');
+console.log('holaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+// import { MongoClient } from "mongodb";
 const { MongoClient } = require("mongodb")
 require('dotenv').config()
 const mongoClient = new MongoClient(process.env.MONGODB_URI)
-const promesa = mongoClient.connect()
+const clientePromesa = mongoClient.connect()
 const manejador = async (event) => {
-    
+    try {
+        const database = (await clientePromesa).db(process.env.MONGODB_DATABASE)
+        const collection = database.collection(process.env.MONGODB_COLLECTION)
+        const resultados = await collection.find({}).limit(10).toArray()
+        console.log(resultados);
+    } catch (error) {
+        return { statusCode: 500, body: error.toString() }
+    }
 }
+manejador()
+// module.exports = { manejador }
+
 // const tabla = document.getElementById('tabla')
-function mostrar() {
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err
-        let transformacion = db.db('estudiantes')
-        transformacion.collection('estudiantes').find(function(err, result) {
-            if (err) throw err
-            console.log(result);
-            // llenar(result)
-            db.close()
-        })
-    })
-}
+// function mostrar() {
+//     MongoClient.connect(url, function(err, db) {
+//         if (err) throw err
+//         let transformacion = db.db('estudiantes')
+//         transformacion.collection('estudiantes').find(function(err, result) {
+//             if (err) throw err
+//             console.log(result);
+//             // llenar(result)
+//             db.close()
+//         })
+//     })
+// }
 // function llenar(objeto) {
 //     let texto = ''
 //     for (const cosa in objeto) {
@@ -31,4 +42,4 @@ function mostrar() {
 //     tabla.innerText += texto
 // }
 
-mostrar()
+// mostrar()
