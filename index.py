dac23c705671951bfd9b6627554ac9243bba63bf
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-08-08 17:34:25 -0400

    aaaaaaaaaaaaaaaaaaaaaaaa

---
+++ index.py
@@ -0,0 +1,8 @@
+import requests
+
+url = 'https://pbs.twimg.com/media/F2-S13FXsAAet9_?format=jpg&name=900x900'
+
+data = requests.get(url).content
+
+with open('nueva.png', 'wb') as handler:
+    handler.write(data)
